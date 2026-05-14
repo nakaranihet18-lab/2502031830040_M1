537586c3042bdc1366d25a5571a45460e6501c3c
nakaranihet18-lab <nakaranihet18@gmail.com>
2026-05-14 12:50:10 +0530

    Add files via upload

---
+++ day 4/javascript.js
@@ -0,0 +1,23 @@
+document.write("<hr>")
+document.write("helloo")
+document.write("<hr>")
+
+document.getElementById("demo").innerHTML = "hello js"
+
+var x=5;
+var y=6;
+var add=x+y;
+var sub=x-y;
+var multi=x*y;
+var divi=x/y;
+
+document.getElementById("demo2").innerHTML = "addition :"+add  +"<br>"+"substrction:"+sub+"<br>"+ "muliplication"+multi+ "<br>"+"division"+divi ;
+
+var y = 10
+var x = "heee"
+var a = false
+var w = [1,2,3]
+
+
+document.getElementById("data").innerHTML=typeof(y)+"<br>"+typeof(x)+"<br>"+typeof(a)+"<br>"+typeof(w)
+
