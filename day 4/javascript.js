0b6962304f53413238cb26695a8e6b1446ddf149
nakaranihet18-lab <nakaranihet18@gmail.com>
2026-05-14 13:51:13 +0530

    Update javascript.js

--- day 4/javascript.js
+++ day 4/javascript.js
@@ -21,3 +21,98 @@ var w = [1,2,3]
 
 document.getElementById("data").innerHTML=typeof(y)+"<br>"+typeof(x)+"<br>"+typeof(a)+"<br>"+typeof(w)
 
+
+for(let i=0;i<=5;i++){
+    document.getElementById("post").innerHTML+= i+" "
+}
+
+for(let i=5;i>=0;i--){
+    document.getElementById("post").innerHTML+= "<br>"+i+" "
+}
+
+
+var cc = 3;
+var tt = 5;
+
+if (cc == tt){
+    document.getElementById("ie").innerHTML = "equal"
+   
+}else
+    {
+    document.getElementById("ie").innerHTML = "not equal"
+} 
+   
+
+let num = 4;
+
+if (num %2 == 0){
+    document.getElementById("ie").innerHTML = "even"
+   
+}else
+    {
+    document.getElementById("ie").innerHTML = "odd "
+}    
+
+
+
+let marks = 50;
+
+if (marks >=30){
+    document.getElementById("mark").innerHTML = "pass"
+   
+}else
+    {
+    document.getElementById("mark").innerHTML = "fail "
+}   
+
+
+
+
+let eli = 20;
+
+if (eli >=18){
+    document.getElementById("eli").innerHTML = "eligiblity"
+   
+}else
+    {
+    document.getElementById("eli").innerHTML = "not eligiblity "
+}   
+
+
+let temp = 45;
+
+if (eli <=40){
+    document.getElementById("temp").innerHTML = "high"
+   
+}else
+    {
+    document.getElementById("temp").innerHTML = "low "
+}  
+
+
+
+let mark = 250;
+
+if (mark >=400){
+    document.getElementById("marks").innerHTML = "A"
+   
+}else if(mark >=300){
+    
+    document.getElementById("marks").innerHTML = "B"
+}
+else{
+    document.getElementById("marks").innerHTML = "c"
+}
+
+
+
+var aa = 25000;
+var bb = 40000;
+
+if (aa>=bb){
+    document.getElementById("bank").innerHTML = "enough"
+   
+}else
+    {
+    document.getElementById("bank").innerHTML = " not enough"
+} 
